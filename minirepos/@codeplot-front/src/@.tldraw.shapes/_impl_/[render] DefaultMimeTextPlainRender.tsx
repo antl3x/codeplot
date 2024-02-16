50551626131c962b50c51f1920a7304b741546d4
Antonio Moura (@antl3x) <aasm.moura@gmail.com>
2024-02-15 17:34:25 -0700

    #0

--- minirepos/@codeplot-front/src/@.tldraw.shapes/_impl_/[render] DefaultMimeTextPlainRender.tsx
+++ minirepos/@codeplot-front/src/@.tldraw.shapes/_impl_/[render] DefaultMimeTextPlainRender.tsx
@@ -10,8 +10,6 @@ export function DefaultMimeTextPlainRender({
   return (
     <div className="overflow-auto bg-[var(--codeplot-surface2-backgroundColor)] w-full h-full p-2">
       <pre>{shape.props.mime["text/plain"]}</pre>
-      <img src="https://via.placeholder.com/150" alt="placeholder" />
-      <h1>Hello world</h1>
     </div>
   );
 }
